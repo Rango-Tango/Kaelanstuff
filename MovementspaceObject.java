74bbc1ec57d8cc2a72e4e9f0214f49a423e03aeb
GhostofHarambe69 <kaelan99@gmail.com>
2017-04-24 08:49:32 -0400

    Add files via upload

---
+++ MovementspaceObject.java
@@ -0,0 +1,23 @@
+
+public class MovementspaceObject {
+
+	int x,y,player;
+	public void setX(int here){
+		this.x=here;
+	}
+	public void setY(int here){
+		this.y=here;
+	}
+	public void setPlayer(int here){
+		this.player=here;
+	}
+	public int getX(){
+		return this.x;
+	}
+	public int getY(){
+		return this.y;
+	}
+	public int getPlayer(){
+		return this.player;
+	}
+}
